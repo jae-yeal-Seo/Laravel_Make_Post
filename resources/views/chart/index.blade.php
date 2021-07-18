76bdb285a832f0fc1cd2113b56206cadceead403
woduf9173@naver.com <woduf9173@naver.com>
2021-07-18 21:35:11 +0900

    차트 블레이드에 navigation추가하기

--- resources/views/chart/index.blade.php
+++ resources/views/chart/index.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.4.1/dist/chart.min.js"></script>
-</head>
-<body>
+<x-app-layout>
+    <x-slot name="header">
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@3.4.1/dist/chart.min.js"></script>
+    </x-slot>
+<div class="py-13">
     <canvas id="myChart" width="600" height="700"></canvas>
     <script>
     var ctx = document.getElementById('myChart').getContext('2d');
@@ -69,5 +64,5 @@
         </a>
         @endforeach
     </div>
-</body>
-</html>
+</div>
+</x-app-layout>
